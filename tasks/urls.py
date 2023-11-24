b729682f4baa62457c58ba44f38b32cd66e9c7cd
Dovud Salimov <a9059262614@gmail.com>
2023-11-24 19:59:12 +0500

    first

---
+++ tasks/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import HomePageView, AboutPageView,PortfolioPageView
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="base"),
+    path("home", HomePageView.as_view(), name="home"),
+    path("about", AboutPageView.as_view(), name="about"),
+    path("portfolio/", PortfolioPageView.as_view(), name="portfolio"),
+    
+
+
+
+
+]
